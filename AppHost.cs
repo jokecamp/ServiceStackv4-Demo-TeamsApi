c5361fe91ec0aca9f3c5e90f6f2bb483afa2d8a4
Joe Kampschmidt <jokecamp@gmail.com>
2013-12-05 16:18:04 -0500

    Added some dynamic routing at runtime
    also updated readme

--- AppHost.cs
+++ AppHost.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Funq;
 using ServiceStack;
 using ServiceStack.Data;
+using ServiceStack.DataAnnotations;
 using ServiceStack.OrmLite;
 
 namespace ServiceStackv4_Demo_TeamsApi
@@ -11,6 +12,10 @@ namespace ServiceStackv4_Demo_TeamsApi
     {
         public AppHost() : base("Test Razor", typeof (AppHost).Assembly)
         {
+            // We can dynamically add routes now. I believe this must be done in the app host constructor. Does not work in the Configure section.
+            // https://github.com/ServiceStack/ServiceStack/wiki/Release-Notes#add-code-first-attributes-at-runtime-de-coupled-from-pocos
+            typeof(Team).AddAttributes(new RouteAttribute("/teams/{Id}"));
+            typeof(GetTeams).AddAttributes(new RouteAttribute("/teams"));
         }
 
         public override void Configure(Container container)
@@ -27,13 +32,17 @@ namespace ServiceStackv4_Demo_TeamsApi
 
         public static void InitData(Container container)
         {
+            // dynamic OrmLite attributes can be added before using the DTO with OrmLite
+            typeof(Team).GetProperty("Id").AddAttributes(new AutoIncrementAttribute());
+            typeof(Team).GetProperty("HideMe").AddAttributes(new IgnoreAttribute());
+
             using (var db = container.Resolve<IDbConnectionFactory>().OpenDbConnection())
             {
                 db.CreateTableIfNotExists<Team>();
 
                 var teams = new List<Team>
                     {
-                        new Team() {Id = 1, Name = "Manchester United", CreatedDate = DateTime.Now},
+                        new Team() {Id = 1, Name = "Manchester United", CreatedDate = DateTime.Now, HideMe = "you shouldn't see this"},
                         new Team() {Id = 2, Name = "Arsenal", CreatedDate = DateTime.Now},
                         new Team() {Id = 3, Name = "Manchester City", CreatedDate = DateTime.Now},
                         new Team() {Id = 4, Name = "Chelsea", CreatedDate = DateTime.Now},
@@ -47,16 +56,18 @@ namespace ServiceStackv4_Demo_TeamsApi
 
     /// <summary>
     /// A sports team DTO
+    /// 
+    /// Clean POCO that has no Attributes or routes if that is your cup of tea
+    /// 
     /// </summary>
-    [Route("/teams/{Id}")]
     public class Team : IReturn<Team>
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime CreatedDate { get; set; }
-    }
 
-    [Route("/teams")]
+        public string HideMe { get; set; }
+    }
     public class GetTeams : IReturn<List<Team>> { }
 
     public class TeamService : Service
--- README.md
+++ README.md
@@ -4,5 +4,12 @@ Demo by Joe Kampschmidt
 
 Simple API for demo of new ServiceStack v4 API. Created to experiment with the new API features.
 
+Some of the features demonstrated with this project:
+- dynamic route adding to DTOs at runtime
+- dynamic OrmLite properties added to DTOs at runtime
+- custom HTML format template used
+- serving static files outside /api level 
+- a few OrmLite query examples
+
 This project uses the "Starter" option seen at https://servicestack.net/pricing. 
 It is subject to the qutoas outlined at https://servicestack.net/download#free-quotas
